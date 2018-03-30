a89f97cf707a58dc2692c1d482c3a1bc7a625c08
kylehassett <hassettkyle@gmail.com>
2018-03-30 13:42:32 -0400

    starting the portfolio page. Will be basically a blog with each post being a work piece that I want to highlight.

---
+++ page-home-content.php
@@ -0,0 +1,30 @@
+<div class="row">
+  <div class="col-sm-6">
+    <div class="form-group">
+      <label for="contact-name" class="sr-only">Name</label>
+      [text* your-name class:form-control class:input-lg id:contact-name placeholder "Your name"]
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="form-group">
+      <label for="contact-email" class="sr-only">Email</label>
+      [email* your-email class:form-control class:input-lg id:contact-email placeholder "Your email"]
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-12">
+    <div class="form-group">
+      <label for="contact-subject" class="sr-only">Subject</label>
+      [text* your-subject class:form-control class:input-lg id:contact-subject placeholder "Your subject"]
+    </div>
+  </div>
+  <div class="col-sm-12">
+    <div class="form-group">
+      <label for="contact-words" class="sr-only">Message</label>
+      [textarea* your-message class:form-control class:input-lg id:contact-words x3 placeholder "Your message"]
+    </div>
+  </div>
+</div>
+<input type="submit" class="btn btn-info btn-lg pull-right" value="Get in touch &raquo;">
+[submit class:btn class:btn-info class:btn-lg class:pull-right "Get in touch"]
